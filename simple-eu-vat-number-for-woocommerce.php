a76f86e3ca07ce54b8ac183f2063c6a145bebfa6
JJMontalban <jjmontalban@gmail.com>
2024-03-12 09:37:30 +0100

    package validation - checkout side - admin side

--- simple-eu-vat-number-for-woocommerce.php
+++ simple-eu-vat-number-for-woocommerce.php
@@ -12,6 +12,9 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Domain Path: lang
 */  
 
+use DragonBe\Vies\Vies;
+use DragonBe\Vies\ViesException;
+
 defined( 'ABSPATH' ) || exit;
 
 class Simple_EU_VAT_Number_For_WooCommerce {
@@ -19,7 +22,7 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     public $allowed_countries;
 
     public function __construct() {
-        //require_once 'vendor/autoload.php'; // to include php-vat-checker library
+        require_once 'vendor/autoload.php';
 
         add_action( 'plugins_loaded', array( $this, 'svnfw_check_dependencies' ) );
         add_action( 'plugins_loaded', array( $this, 'svnfw_load_plugin_textdomain' ) );
@@ -27,18 +30,19 @@ class Simple_EU_VAT_Number_For_WooCommerce {
         add_action('wp_enqueue_scripts', array($this, 'svnfw_enqueue_custom_scripts'));
         //checkout
         add_action( 'woocommerce_checkout_fields', array( $this, 'svnfw_add_vat_checkout' ) );
-        add_action( 'woocommerce_checkout_process', array( $this, 'svnfw_validate_vat_checkout' ) );
+        add_action( 'woocommerce_after_checkout_validation', array( $this, 'svnfw_validate_vat_checkout' ), 10, 2 );
+        add_action( 'woocommerce_created_customer', array( $this, 'svnfw_save_vat_registration' ) );
         add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'svnfw_save_vat_checkout' ) );
-        //order
-        add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'svnfw_add_vat_order' ), 10, 1 );
         //account
         add_filter('woocommerce_billing_fields', array($this, 'svnfw_add_vat_account'));
-        add_action('woocommerce_after_save_address_validation', array($this, 'svnfw_validate_vat_account'), 10, 3);
-        add_action('woocommerce_save_account_details', array( $this, 'svnfw_save_vat_account' ) );
+        add_action('woocommerce_after_save_address_validation', array($this, 'svnfw_save_vat_account'), 10, 2);
         //admin
         add_filter('woocommerce_customer_meta_fields', array($this, 'svnfw_add_vat_admin'));
-        add_action('edit_user_profile_update', array($this, 'svnfw_validate_vat_admin'));
         add_action('edit_user_profile_update', array($this, 'svnfw_save_vat_admin'));
+        //order
+        add_action( 'woocommerce_admin_order_data_after_billing_address', array( $this, 'svnfw_add_vat_order' ), 10, 1 );
+        //email
+        add_filter( 'woocommerce_email_order_meta_fields',  array( $this, 'svnfw_add_vat_email' ), 10, 3 );
     }
 
 
@@ -57,7 +61,6 @@ class Simple_EU_VAT_Number_For_WooCommerce {
         }
     }
 
-
     /**
      * Loads the plugin text domain for translation.
      *
@@ -69,7 +72,6 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     public function svnfw_load_plugin_textdomain() {
         load_plugin_textdomain( 'svnfw', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
     }
-
 
     /**
      * Defines the allowed countries for the plugin.
@@ -90,7 +92,6 @@ class Simple_EU_VAT_Number_For_WooCommerce {
         }
     }
 
-
     /**
      * Enqueues custom scripts for the plugin.
      *
@@ -109,6 +110,9 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     }
 
 
+    //////////////////////CHECKOUT 
+
+
     /**
      * Displays the VAT number field at checkout.
      *
@@ -121,7 +125,7 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     public function svnfw_add_vat_checkout( $fields ) {
         $fields['billing']['vat_number'] = array(
             'label'     => __('VAT Number', 'svnfw'),
-            'placeholder'   => _x('VAT Number', 'placeholder', 'svnfw'),
+            'placeholder'   => __('VAT Number', 'placeholder', 'svnfw'),
             'required'  => false,
             'class'     => array('form-row-wide'),
             'clear'     => true,
@@ -130,6 +134,107 @@ class Simple_EU_VAT_Number_For_WooCommerce {
 
         return $fields;
     }
+
+    /**
+     * Validates the VAT number during checkout.
+     *
+     * This method is used to validate the VAT number entered at checkout.
+     * If the VAT number is not valid, it adds an error message which prevents the checkout from being completed.
+     *
+     * @param array $data An array of posted data.
+     * @param object $errors A WP_Error object containing any errors encountered during validation.
+     */
+    public function svnfw_validate_vat_checkout( $data, $errors ) {
+        if (in_array(WC()->customer->get_billing_country(), $this->allowed_countries)) {
+            $vat_number = isset($data['vat_number']) ? $data['vat_number'] : '';
+            if (!empty($vat_number) && !$this->svnfw_is_valid_vat_number($vat_number)) {
+                $errors->add( 'validation', '<strong>' . __( 'Error', 'svnfw' ) . '</strong>: ' . __( 'Invalid VAT Number.', 'svnfw' ) );
+            }
+        }
+    }
+
+    /**
+     * Saves the VAT number when a new user account is created at checkout.
+     *
+     * This method is used to save the VAT number to the user meta when a new user account is created during checkout.
+     * This allows the VAT number to be associated with the user for future orders.
+     *
+     * @param int $customer_id The ID of the customer who has just been created.
+     */
+    public function svnfw_save_vat_registration( $customer_id ) {
+        if ( isset( $_POST['vat_number'] ) ) {
+            $vat_number = sanitize_text_field( $_POST['vat_number'] );
+            if ( !empty( $vat_number ) ) {
+                update_user_meta( $customer_id, 'vat_number', $vat_number );
+            }
+        }
+    }
+    
+
+    /**
+     * Saves the VAT number to the order meta after checkout.
+     *
+     * This method is used to save the VAT number to the order meta after a successful checkout.
+     * The VAT number is saved in the order meta so it can be used for order processing and reporting.
+     *
+     * @param int $order_id The ID of the order that has just been created.
+     */    
+    public function svnfw_save_vat_checkout($order_id) {
+        if (in_array(WC()->customer->get_billing_country(), $this->allowed_countries)) {
+            $vat_number = isset($_POST['vat_number']) ? $_POST['vat_number'] : '';
+            if (!empty($vat_number)) {
+                update_post_meta($order_id, '_vat_number', sanitize_text_field($vat_number));
+            }
+        }
+    }
+
+
+
+    ///////////////////////////////////ACCOUNT
+
+    /**
+     * Adds the VAT number field to the account page.
+     *
+     * This method is used to add the VAT number field to the account page.
+     *
+     * @param array $fields Existing account page fields.
+     * @return array $fields Modified account page fields.
+     */
+    public function svnfw_add_vat_account( $fields ) {
+        $fields['vat_number'] = array(
+            'label'     => __('VAT Number', 'svnfw'),
+            'placeholder'   => __('VAT Number', 'placeholder', 'svnfw'),
+            'description' => '',
+            'priority' => 35
+        );
+        return $fields;
+    }
+
+
+    public function validate_vat_number($vat_number) {
+        if (!empty($vat_number) && !$this->svnfw_is_valid_vat_number($vat_number)) {
+            return new WP_Error('invalid_vat_number', __('Invalid VAT Number.', 'svnfw'));
+        }
+        return true;
+    }
+    
+    public function svnfw_save_vat_account($user_id, $load_address) {
+        if ($load_address === 'billing' && isset($_POST['vat_number'])) {
+            $vat_number = $_POST['vat_number'];
+            if (!empty($vat_number)) {
+                $validation_result = $this->validate_vat_number($vat_number);
+                if (is_wp_error($validation_result)) {
+                    wc_add_notice($validation_result->get_error_message(), 'error');
+                    return; // No guardes el valor si la validación falla
+                }
+                update_user_meta($user_id, 'vat_number', sanitize_text_field($vat_number));
+            }
+        }
+    }
+    
+    
+    
+   ////////////////////////////////////////////////////////////ADMIN
 
 
     /**
@@ -148,78 +253,38 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     
         return $fields;
     }
-    
-    
-    /**
-     * Validates the VAT number at checkout.
-     *
-     * This method is used to validate the VAT number entered at checkout. 
-     * If the VAT number is not valid, an error notice is added.
-     *
-     * @return void
-     */
-    public function svnfw_validate_vat_checkout() {
-        if ( in_array( WC()->customer->get_billing_country(), $this->allowed_countries ) ) {
-            $vat_number = isset( $_POST['vat_number'] ) ? $_POST['vat_number'] : '';
-            if ( ! empty( $vat_number ) && ! $this->svnfw_is_valid_vat_number( $vat_number ) ) {
-                // Agrega un aviso de error
-                wc_add_notice( __('Invalid VAT Number.', 'svnfw'), 'error' );
-                // Agrega la clase 'input-error' al campo del número de IVA
-                wc_add_notice('<script>jQuery("#vat_number").addClass("input-error");</script>', 'error');
-            }
-        }
-    }
-    
-
-
-    public function svnfw_validate_vat_account($user_id, $load_address, $errors) {
-        $vat_number = isset($_POST['vat_number']) ? $_POST['vat_number'] : '';
-        if (!empty($vat_number) && !$this->svnfw_is_valid_vat_number($vat_number)) {
-            // Agrega un aviso de error
-            $errors->add('vat_number', __('Invalid VAT Number.', 'svnfw'));
-            // Agrega la clase 'input-error' al campo del número de IVA
-            wc_add_notice('<script>jQuery("#vat_number").addClass("input-error");</script>', 'error');
-        }
-    }
-    
-    
-
 
     /**
-     * Validates and saves the VAT number from the admin user profile page.
+     * Validates the VAT number from the admin user profile page.
+     * If the VAT number is not valid, it adds an error and prevents the user metadata from being saved.
+     * If the VAT number is valid, it saves the number in the user metadata.
      *
      * @param int $user_id The ID of the user being saved.
      */
     public function svnfw_save_vat_admin($user_id) {
         if (isset($_POST['vat_number'])) {
             $vat_number = sanitize_text_field($_POST['vat_number']);
-            // Verifica si el número de IVA es válido
+    
             if (!$this->svnfw_is_valid_vat_number($vat_number)) {
-                // Si el número de IVA no es válido, muestra un mensaje de error
                 add_action('user_profile_update_errors', function($errors) {
-                    $errors->add('vat_number', __('Invalid VAT Number.', 'svnfw'));
-                });
+                    $errors->add('vat_number', '<strong>' . __('Error', 'svnfw') . '</strong>: ' . __('Invalid VAT Number.', 'svnfw'));
+                }, 10, 3);
+    
+                add_filter('update_user_metadata', function($null, $object_id, $meta_key, $meta_value) use ($user_id, $vat_number) {
+                    if ($object_id == $user_id && $meta_key == 'vat_number' && $meta_value == $vat_number) {
+                        return false;
+                    }
+                }, 10, 4);
             } else {
-                // Si el número de IVA es válido, guárdalo en los metadatos del usuario
                 update_user_meta($user_id, 'vat_number', $vat_number);
             }
         }
     }
-
-    /**
-     * Saves the VAT number after checkout.
-     *
-     * This method is used to save the VAT number to the order meta after checkout.
-     *
-     * @param int $order_id The ID of the order.
-     * @return void
-     */
-    public function svnfw_save_vat_checkout( $order_id ) {
-        if ( ! empty( $_POST['vat_number'] ) ) {
-            update_post_meta( $order_id, '_vat_number', sanitize_text_field( $_POST['vat_number'] ) );
-        }
-    }
-
+    
+    
+    
+    //////////////////////////////////ORDER
+    
 
     /**
      * Displays the VAT number in the admin order page.
@@ -234,38 +299,32 @@ class Simple_EU_VAT_Number_For_WooCommerce {
     }
 
 
+
+
+    /////////////////////////EMAIL
+
+
     /**
-     * Adds the VAT number field to the account page.
+     * Adds the VAT number to order emails.
      *
-     * This method is used to add the VAT number field to the account page.
+     * This function hooks into the 'woocommerce_email_order_meta_fields' filter,
+     * which is used to add additional meta fields to order emails.
      *
-     * @param array $fields Existing account page fields.
-     * @return array $fields Modified account page fields.
+     * @param array $fields The existing meta fields that are added to order emails.
+     * @param bool $sent_to_admin Whether the email is being sent to the admin.
+     * @param WC_Order $order The order object.
+     * @return array $fields The modified meta fields.
      */
-    public function svnfw_add_vat_account( $fields ) {
+    public function svnfw_add_vat_email( $fields, $sent_to_admin, $order ) {
         $fields['vat_number'] = array(
-            'label'     => __('VAT Number', 'svnfw'),
-            'placeholder'   => _x('VAT Number', 'placeholder', 'svnfw'),
-            'description' => '',
-            'priority' => 35
+            'label' => __( 'VAT Number', 'svnfw' ), // The field name
+            'value' => get_post_meta( $order->get_id(), '_vat_number', true ),
         );
+
         return $fields;
     }
 
-
-    /**
-     * Saves the VAT number to the user profile.
-     *
-     * This method is used to save the VAT number to the user profile.
-     *
-     * @param int $user_id The ID of the user.
-     * @return void
-     */
-    public function svnfw_save_vat_account($user_id) {
-        if (isset($_POST['vat_number'])) {
-            update_user_meta($user_id, 'vat_number', sanitize_text_field($_POST['vat_number']));
-        }
-    }
+    ////////////////VALIDATION
 
     /**
      * Checks if a VAT number is valid.
@@ -276,9 +335,23 @@ class Simple_EU_VAT_Number_For_WooCommerce {
      * @return bool True if the VAT number is valid, false otherwise.
      */
     public function svnfw_is_valid_vat_number( $vat_number ) {
-        //todo
+        /* 
+        $vies = new Vies();
+        $countryCode = substr($vat_number, 0, 2);
+        $vatNumber = substr($vat_number, 2);
+
+        try {
+            $result = $vies->validateVat($countryCode, $vatNumber);
+            return $result->isValid();
+        } catch (ViesException $e) {
+            return false;
+        } 
+        */
+
         return true;
     }
+
+
 
 }
 
