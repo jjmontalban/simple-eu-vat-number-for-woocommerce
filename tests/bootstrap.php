094a5e812cf68607ab57e0a6c73b2589bed184ef
JJMontalban <jjmontalban@gmail.com>
2024-03-13 15:52:45 +0100

    Remove tests/ directory

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Define path to WordPress install
-define('ABSPATH', 'C:/wamp64/www/pqs/');
-
-// Load WordPress
-require_once ABSPATH . 'wp-load.php';
-
-// Load WooCommerce
-require_once ABSPATH . 'wp-content/plugins/woocommerce/woocommerce.php';
