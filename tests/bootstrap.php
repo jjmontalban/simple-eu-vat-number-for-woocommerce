33f206ef2af6205417a3a6056caba2fe02c67a73
JJMontalban <jjmontalban@gmail.com>
2024-03-13 15:51:35 +0100

    testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+// Define path to WordPress install
+define('ABSPATH', 'C:/wamp64/www/pqs/');
+
+// Load WordPress
+require_once ABSPATH . 'wp-load.php';
+
+// Load WooCommerce
+require_once ABSPATH . 'wp-content/plugins/woocommerce/woocommerce.php';
